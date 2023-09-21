0ec2412a00e228ca242feaf76fafc6a18c37b88a
mkaraki <26180919+mkaraki@users.noreply.github.com>
2023-09-21 12:58:39 +0900

    Add bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css'
+  ],
+  plugins: [
+    '~/plugins/bootstrap.client.ts'
+  ],
 })
